5a954758eed251d5adce4f35ecf17ef2931f8df5
dimpram101 <dimas2004211@gmail.com>
2024-03-09 10:59:04 +0800

    fix: remove sidebar context

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import DashboardLayout from "@layouts/DashboardLayout";
 import DashboardIndex from "@pages/dashboard/DashboardIndex";
-import OpenSidebarProvider from "@contexts/providers/OpenSidebarProvider";
 import UserList from "@pages/dashboard/users/UserList";
 import UserAdd from "@pages/dashboard/users/UserAdd";
 import PlaneTicketList from "@pages/dashboard/plane-tickets/PlaneTicketList";
@@ -86,7 +85,7 @@ const router = createBrowserRouter([
           {
             path: "edit/:partner_id",
             element: <PartnerEdit />,
-          }
+          },
         ],
       },
     ],
@@ -98,11 +97,7 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return (
-    <OpenSidebarProvider>
-      <RouterProvider router={router} />
-    </OpenSidebarProvider>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
