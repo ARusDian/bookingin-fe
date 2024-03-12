f6b49a377200446e8ae1043890e5d8638f05a25f
dimpram101 <dimas2004211@gmail.com>
2024-03-12 22:30:48 +0800

    feat: add show hotel detail

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import { ToastContainer } from "react-toastify";
 import LogList from "@pages/dashboard/admin/log/LogList";
 import HotelList from "@pages/dashboard/partner/hotel/HotelList";
 import HotelCreate from "@pages/dashboard/partner/hotel/HotelCreate";
+import HotelDetail from "@pages/dashboard/partner/hotel/HotelDetail";
 
 const queryClient = new QueryClient();
 
@@ -110,6 +111,10 @@ const router = createBrowserRouter([
                 path: "create",
                 element: <HotelCreate />,
               },
+              {
+                path: ":hotel_id",
+                element: <HotelDetail />,
+              }
             ],
           },
         ],
