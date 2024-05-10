422a700d30035efd61d356cc3c0bfdcdfbb2f68e
dimpram101 <dimas2004211@gmail.com>
2024-05-10 19:35:22 +0800

    fix: update routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,6 @@ import { HelmetProvider } from "react-helmet-async";
 import { ToastContainer } from "react-toastify";
 import Home from "@pages/homes/Home";
 import "./App.css";
-import DashboardLayout from "@layouts/DashboardLayout";
-import DashboardIndex from "@pages/dashboard/DashboardIndex";
-import OpenSidebarProvider from "@contexts/providers/OpenSidebarProvider";
-import UserList from "@pages/dashboard/users/UserList";
-import UserAdd from "@pages/dashboard/users/UserAdd";
-import PlaneTicketList from "@pages/dashboard/plane-tickets/PlaneTicketList";
-import HotelTicketList from "@pages/dashboard/hotel-tickets/HotelTicketList";
-import PartnerList from "@pages/dashboard/partners/PartnerList";
 import Flight from "@pages/homes/flights/Flight";
 import Hotel from "@pages/homes/hotels/Hotel"; 
 import FlightPayments from "@pages/homes/flights/payments/FlightPayments";
@@ -53,57 +45,6 @@ const router = createBrowserRouter([
       {
         path: "payments",
         element: <FlightPayments />,
-      },
-    ],
-  },
-  
-  {
-    path: "/dashboard",
-    element: <DashboardLayout />,
-    children: [
-      {
-        index: true,
-        element: <DashboardIndex />,
-      },
-      {
-        path: "users",
-        children: [
-          {
-            index: true,
-            element: <UserList />,
-          },
-          {
-            path: "create",
-            element: <UserAdd />,
-          },
-        ],
-      },
-      {
-        path: "plane-tickets",
-        children: [
-          {
-            index: true,
-            element: <PlaneTicketList />
-          }
-        ]
-      },
-      {
-        path: "hotel-tickets",
-        children: [
-          {
-            index: true,
-            element: <HotelTicketList />
-          }
-        ]
-      },
-      {
-        path: "partners",
-        children: [
-          {
-            index: true,
-            element: <PartnerList />
-          }
-        ]
       },
     ],
   },
